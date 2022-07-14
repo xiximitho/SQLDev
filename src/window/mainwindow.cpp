dbc6a5207823321cea9037670ae782870ca4f939
Felipe Mattes <mattesfelipe@gmail.com>
2022-07-13 21:40:23 -0300

    Criado menus

--- src/window/mainwindow.cpp
+++ src/window/mainwindow.cpp
@@ -3,9 +3,69 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    setProps();
+    createActions();
+    createMenus();
 }
 
 MainWindow::~MainWindow()
 {
 }
 
+void MainWindow::setProps()
+{
+    this->setWindowTitle("SQL Dev");
+    this->setMinimumSize(1280,720);
+    this->adjustSize();
+}
+
+void MainWindow::createMenus()
+{
+    /*Menu Arquivo*/
+    fileMenu = menuBar()->addMenu(tr("&Arquivo"));
+    fileMenu->addAction(newAct);
+    fileMenu->addAction(openAct);
+
+    /*Menu Editar*/
+    editMenu = menuBar()->addMenu(tr("&Editar"));
+
+    /*Menu Ajuda*/
+    helpMenu = menuBar()->addMenu(tr("&Ajuda"));
+
+    /*Menu Formatar*/
+    formatMenu = editMenu->addMenu(tr("&Formatar"));
+    formatMenu->addAction(identAct);
+}
+
+void MainWindow::createActions()
+{
+    newAct = new QAction(tr("&Novo"), this);
+    newAct->setShortcuts(QKeySequence::New);
+    newAct->setStatusTip(tr("Cria um novo arquivo"));
+    connect(newAct, &QAction::triggered, this, &MainWindow::newFile);
+
+    openAct = new QAction(tr("&Abrir"), this);
+    openAct->setShortcuts(QKeySequence::Open);
+    openAct->setStatusTip(tr("Abrir um arquivo"));
+    connect(openAct, &QAction::triggered, this, &MainWindow::open);
+
+    identAct = new QAction(tr("&Identar"), this);
+    identAct->setShortcuts(QKeySequence::Italic);
+    identAct->setStatusTip(tr("Identa o arquivo"));
+    connect(identAct, &QAction::triggered, this, &MainWindow::ident);
+}
+
+void MainWindow::newFile()
+{
+
+}
+
+void MainWindow::open()
+{
+
+}
+
+void MainWindow::ident()
+{
+
+}
--- src/window/mainwindow.h
+++ src/window/mainwindow.h
@@ -3,7 +3,9 @@
 
 #include <QMainWindow>
 #include <QPushButton>
-
+#include <QMenuBar>
+#include <QMenu>
+#include <QLabel>
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -11,5 +13,26 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+private:
+    void setProps();
+    void createMenus();
+    void createActions();
+    /*Variaveis/Atributos locais*/
+
+    QMenu *fileMenu;
+    QMenu *editMenu;
+    QMenu *formatMenu;
+    QMenu *helpMenu;
+    QActionGroup *alignmentGroup;
+    QAction *newAct;
+    QAction *openAct;
+    QAction *identAct;
+
+
+
+private slots:
+    void newFile();
+    void open();
+    void ident();
 };
 #endif // MAINWINDOW_H
